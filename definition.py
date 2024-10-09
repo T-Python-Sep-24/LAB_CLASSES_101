00c1990697e1901ae2dc96f3e6c0706c30c8d404
Alhanouf <hnno3040@gmail.com>
2024-10-09 22:53:17 +0300

    done

---
+++ definition.py
@@ -0,0 +1,13 @@
+class Panda:
+    def __init__(self, name, age, weight, height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+    
+    def weight1(self):
+        print(f"{self.name} is {self.weight} kg.")
+
+    def height1(self):
+        print(f"{self.name} is {self.height} cm.")
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from definition import Panda
+
+
+person1 = Panda("li", 23, 60, 170)
+person2 = Panda("Po", 25, 50, 160)
+
+print(person1.name)
+person1.height1()
+person2.weight1()
